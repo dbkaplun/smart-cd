590056bfa5c7e725ab087b9b91ff7546d69331d6
Dan Kaplun <dan@secrettriangle.net>
2015-01-05 06:48:49 -0600

    Improves compatibility with other scripts using chpwd

--- smart-cd.zsh
+++ smart-cd.zsh
@@ -3,8 +3,8 @@
 [ ! -n "${SMART_CD_LS+1}" ] && SMART_CD_LS=true
 [ ! -n "${SMART_CD_GIT_STATUS+1}" ] && SMART_CD_GIT_STATUS=true
 
-_lastgitdir=''
-chpwd () {
+_smart_cd_lastgitdir=''
+_smart_cd_chpwd_handler () {
   emulate -L zsh
 
   [[ $SMART_CD_LS == true ]] && ls -A
@@ -13,9 +13,12 @@ chpwd () {
     local gitdir="$(git rev-parse --git-dir 2>/dev/null)"
     if [[ -n "$gitdir" ]]; then
       gitdir="$gitdir:A" # absolute path of $gitdir
-      [[ "$gitdir" != "$_lastgitdir" ]] && (echo; git status)
-      _lastgitdir="$gitdir"
+      [[ "$gitdir" != "$_smart_cd_lastgitdir" ]] && (echo; git status)
+      _smart_cd_lastgitdir="$gitdir"
     fi
   fi
 }
-chpwd
+
+autoload -U add-zsh-hook
+add-zsh-hook chpwd _smart_cd_chpwd_handler
+_smart_cd_chpwd_handler
